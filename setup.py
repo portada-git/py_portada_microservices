5f622fae73312a27f502b37338c46e7972541b5c
jcanell4 <jcanell4@xtec.cat>
2024-03-04 23:43:22 +0100

    first commit

--- setup.py
+++ setup.py
@@ -9,14 +9,12 @@ setup(name='portadaMicroservices',
     url="https://github.com/portada-git/py_portada_microservices",
     packages=['portada_microservices.services', 'portada_microservices.client'],
     install_requires=[
-	imageio[pyav],
-	imageio[opencv],
-	flask~=3.0.2
-	requests~=2.31.0
-	pyjwt
-	flask-reuploads
-	py_portada_image @ git+https://github.com/portada-git/py_portada_image#egg=py_portada_image,
-	werkzeug~=3.0.1
+	'flask',
+	'requests',
+	'pyjwt',
+	'flask-reuploads',
+	'py_portada_image @ git+https://github.com/portada-git/py_portada_image#egg=py_portada_image',
+	'werkzeug',
     ],
     python_requires='>=3.9',
     zip_safe=False)
