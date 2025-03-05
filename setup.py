bff7035e64ce37fda8d22ab9daa488fae9fffaaa
jcanell4 <jcanell4@xtec.cat>
2025-03-05 20:32:55 +0100

    new functionality: openai_extractor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='portadaMicroservices',
-    version='0.1.28',
+    version='0.1.29',
     description='microservices for PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
@@ -18,7 +18,8 @@ setup(name='portadaMicroservices',
 	'configparser',
     'py_portada_order_blocks @ git+https://github.com/portada-git/py_order_text_blocks#egg=py_portada_order_blocks',
     'cryptography',
-    'py_portada_paragraphs @ git+https://github.com/portada-git/py_portada_paragraphs#egg=py_portada_paragraphs'
+    'py_portada_paragraphs @ git+https://github.com/portada-git/py_portada_paragraphs#egg=py_portada_paragraphs',
+    'py_openai_extractor @ git+https://github.com/portada-git/py_portada_openai_extractor#egg=py_openai_extractor'
     ],
     python_requires='>=3.9',
     zip_safe=False)
